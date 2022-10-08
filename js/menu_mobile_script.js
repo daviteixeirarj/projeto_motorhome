e60f49833d046fe551b28b02948844abb54e6f1b
Davi Teixeira <daviteixeira.devcode@gmail.com>
2022-10-08 11:45:01 -0300

    finalizei nada kkkkk

--- js/menu_mobile_script.js
+++ js/menu_mobile_script.js
@@ -1,10 +1,12 @@
 const btnMobile = document.getElementById('btn-mobile')
 const hamburguer = document.getElementById('hamburguer')
 const menuItems = document.getElementById('menu-itens')
+const main = document.getElementById('main')
 
 function toggleMenu(){
     hamburguer.classList.toggle('active')
     menuItems.classList.toggle('show')
+    main.classList.toggle('noshow')
 }
 
 btnMobile.addEventListener('click',toggleMenu)
